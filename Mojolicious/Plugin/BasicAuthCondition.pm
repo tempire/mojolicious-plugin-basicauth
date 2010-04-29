72578dd7cd79b4814e22288a83ffac7f3e7ef5f0
Glen Hinkle <glen@empireenterprises.com>
2010-04-28 21:19:22 -0400

    Fixed pod

--- Mojolicious/Plugin/BasicAuthCondition.pm
+++ Mojolicious/Plugin/BasicAuthCondition.pm
@@ -75,7 +75,7 @@ Mojolicious::Plugin::BasicAuthCondition - Basic HTTP Auth Condition Plugin
     } ) => sub {...};
     
 	 # To supply only a password (no username)
-	 get '/' => (basic_auth => 'password' ] ) => sub {...};
+	 get '/' => (basic_auth => 'password' ) => sub {...};
 
 =head1 DESCRIPTION
 
